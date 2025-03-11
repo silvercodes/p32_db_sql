c853f1ffe4d037d8f1bab918c3bea332d634e5fb
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-03-12 00:24:26 +0300

    add: sql

---
+++ 03_sql_intro.sql
@@ -0,0 +1,31 @@
+-- comment
+/*
+	comment
+*/
+
+--DECLARE @a int;
+--SET @a = 12;
+
+--DECLARE @b int = 45;
+
+--+ - * / %
+
+--AND OR NOT
+
+--IN ANY ...
+
+--IF
+
+--CASE
+
+--WHILE
+
+--COUNT() MAX() MIN() SUM() ABS()...
+
+
+-- Группы операторов (преложений) --
+-- DML (Data Manipulation Language) SELECT INSERT UPDATE DELETE MERGE ...
+-- DDL (Data Definition Language) CREATE ALTER DROP ...
+-- DCL (Data Control Language) GRANT DENY REVOKE ...
+
+
